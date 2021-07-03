47c0bee10a4b3da0521d835ce3ac1cb9261eb48d
katiepdx <61799774+katiepdx@users.noreply.github.com>
2021-07-02 18:27:37 -0700

    add click counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+// import socketio - https://socket.io/docs/v4/client-initialization/
+import { io } from 'socket.io-client';
+const socket = io(process.env.REACT_APP_SOCKET_URL, {
+  withCredentials: true
+})
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // get current count from socket server
+    socket.on('current-count', (payload) => {
+      setCount(payload)
+    })
+
+    // listen for click updates from server from other clients
+    socket.on('increment-click', (payload) => {
+      console.log(`FE: ${socket.id} clicked ${payload} times`)
+      // updates the state count with incoming clicks
+      setCount(payload)
+    })
+    
+  }, [count])
+
+  const handleClick = () => {
+    // send updated click count to server
+    socket.emit('user-click')
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Count: {count}</p>
+        <button onClick={handleClick}>Click Counter</button>
       </header>
     </div>
   );
